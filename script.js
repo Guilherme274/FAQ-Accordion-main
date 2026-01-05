005073b31672d1223e523a6b0f045bd367600217
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2026-01-05 14:00:46 -0300

    Ajustando script para alteração de imagem ao expandir seção

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 "use strict"
 
 
-let perguntas = document.querySelectorAll(".main_pergunta_resposta");
+let perguntas_respostas = document.querySelectorAll(".main_pergunta_resposta");
+let imgs = document.querySelectorAll(".main_botaoFAQ")
 
-perguntas.forEach((pergunta) => {
-    pergunta.addEventListener("click", () => {
+    perguntas_respostas.forEach((pergunta_resposta) => {
 
-        pergunta.classList.toggle("active");
+        const pergunta = pergunta_resposta.querySelector(".main_pergunta");
+
+        let apertou = false; 
+    
+        pergunta.addEventListener("click", () => {
+
+            const img = pergunta.querySelector("img");
+
+            if(apertou == false) {
+                apertou = true;
+                img.src = "/assets/images/icon-minus.svg";
+            }else {
+                apertou = false;
+                img.src = "/assets/images/icon-plus.svg";
+            }   
+                
+            pergunta_resposta.classList.toggle("active");
+        });
+
     });
-});
+    
+    
+    
 
 
